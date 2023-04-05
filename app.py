11128b200a6e947126169c7529c6d1b3da263c2a
Soumo <soumeswarbanerjee@gmail.com>
2023-04-05 17:35:14 +0530

    modified app.py add a new folder in root directory called static and images uploaded to save the uploded images

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pickle
 import cv2
 import numpy as np
 from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import base64
 
 app = Flask(__name__)
 
@@ -12,9 +14,12 @@ with open('scaler.pkl', 'rb') as f:
 with open('clf.pkl', 'rb') as f:
     clf = pickle.load(f)
 
+app.config['IMAGES_UPLOADS'] = 'static/images_uploaded'
+
 # Define a function to make predictions on uploaded images
 def predict_images(files):
     predictions = []
+    images = []
     for file in files:
         # Read image and preprocess it
         image_data = file.read()
@@ -32,8 +37,10 @@ def predict_images(files):
 
         # Save prediction and image data as a tuple
         predictions.append(prediction_label)
+        image_64_encode = base64.b64encode(image_data).decode('utf-8')
+        images.append('data:image/png;base64,' + image_64_encode)
 
-    return predictions
+    return predictions, images
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -41,10 +48,14 @@ def index():
     if request.method == 'POST':
         # Get uploaded images and make predictions
         files = request.files.getlist('files')
-        predictions = predict_images(files)
+        print(files)
+        for i in files:
+            if i.filename == '':
+                return render_template('index.html')
+        predictions, images = predict_images(files)
 
-        # Render results template with predictions
-        return render_template('results.html', predictions=predictions)
+        # Render results template with predictions and images
+        return render_template('results.html', predictions=predictions, images=images)
 
     # Render index template
     return render_template('index.html')
@@ -52,4 +63,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
